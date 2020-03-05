5b04e05f58d11779de4cb9eedfc52816a0e76828
Hannah Sanderson <hsbuchner@gmail.com>
2020-03-05 00:27:31 -0500

    work on creating the cart

---
+++ client/components/cart.js
@@ -0,0 +1,122 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {getCart, addToCart, removeFromCart, deleteFromCart} from '../store/cart'
+
+const Cart = props => {
+  const bouquets = props.bouquets
+
+  return (
+    <div>
+      <div>
+        <div className="container">
+          <div className="row">
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Product</th>
+                          <th>Name</th>
+                          <th>Price</th>
+                          <th>Qty</th>
+                          <th>Amount</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {props.cart.length &&
+                          props.cart.map((item, index) => (
+                            <tr key={item.id}>
+                              <td>
+                                <Link to={`/bouquets/${item.bouquet.id}`}>
+                                  {item.bouquet.name}
+                                </Link>
+                              </td>
+
+                              <td>
+                                <small>&#36;</small>
+                                {item.bouquet.price}
+                              </td>
+                              <td>
+                                {item.quantity}
+                                <div>
+                                  <button
+                                    type="submit"
+                                    onClick={() => props.handleDecrease(item)}
+                                  >
+                                    remove
+                                  </button>
+                                  <button
+                                    type="submit"
+                                    onClick={() => props.handleIncrease(item)}
+                                  >
+                                    add{' '}
+                                  </button>
+                                </div>
+                              </td>
+                              <td>
+                                <small>&#36;</small>
+                                {item.bouquet.price * item.quantity}
+                              </td>
+                              <td>
+                                <button
+                                  type="submit"
+                                  onClick={() => props.handleDelete(item)}
+                                 />
+                              </td>
+                            </tr>
+                          ))}
+                        <tr>
+                          <td>Total</td>
+                          <td>
+                            <small>$</small>
+                            {props.cart
+                              .map(el => el.bouquet.price * el.quantity)
+                              .reduce((a, b) => a + b, 0)}
+                          </td>
+                          <td>
+                            <Link to="/checkout">
+                              <button type="button">Complete Purchase</button>
+                            </Link>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const mapPropToCart = state => {
+  return {
+    cart: state.currentCart
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    // handleDelete(index){
+    // 	dispatch(deleteFromCart(index))
+    // },
+    handleIncrease(bouquet) {
+      dispatch(addToCart(bouquet))
+    }
+    // handleDecrease(bouquet){
+    // 	dispatch(removeFromCart(bouquet))
+    // },
+    // handleDelete(bouquet){
+    // 	dispatch(deleteFromCart(bouquet))
+    // }
+  }
+}
+
+export default connect(mapPropToCart, mapDispatch)(Cart)
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -19,6 +19,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
+          <Link to="/cart">Cart</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
         </div>
