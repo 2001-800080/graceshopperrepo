4a352ebb63c395638ca1c68c6bf2d9c0c8bca5f1
Ann-Marie Kemp <aedalenb@gmail.com>
2020-03-06 19:54:20 -0500

    commented out selforadmin in cart.js and removed middleware from route to fix issue

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const {User, Order, Bouquet} = require('../db/models')
 
-function isSelfOrAdmin(req, res, next) {
-  if (req.params.id === req.user.id || req.user.isAdmin) return next()
-}
+// function isSelfOrAdmin(req, res, next) {
+//   if (req.params.id === req.user.id || req.user.isAdmin) return next()
+// }
 
-router.put('/:userId/cart', isSelfOrAdmin, async (req, res, next) => {
+router.put('/:userId/cart', async (req, res, next) => {
   try {
     console.log('req.params.userId', req.params.userId)
     console.log('doing router.put in cart.js!!!!!!><><><><><><><')
