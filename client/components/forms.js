21326175db3142e439b1adcd1628f2c84e87452f
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-03-08 17:10:27 -0400

    fixed name

--- client/components/forms.js
+++ client/components/forms.js
@@ -7,7 +7,7 @@ class Form extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="order">Information</h1>
+        <h1 className="order">CheckOut Information</h1>
         <center>
           <img
             src="https://cdn.clipart.email/04cb6ba67cc9e24be570ccfeb690eab5_library-of-book-divider-clip-art-freeuse-library-png-files-_1925-284.png"
