a16a05c5e20cdf41eda1dcf0f30c37313d808013
Nuria Rodriguez <nurialuzr1@gmail.com>
2021-10-27 01:28:05 -0400

    changes

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,18 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
-let pg = require('pg')
-pg.defaults.ssl = false;
+
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+      }
+    },
     logging: false
   }
 )
